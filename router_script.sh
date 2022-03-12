5012b9a3b3dc0939f74624e20c7d63c6d7194006
RhyannaOlson <s3738730@students.fscj.edu>
2022-03-12 08:04:11 -0800

    moved 'output = .." to below connection to try and avoid error

--- router_script.sh
+++ router_script.sh
@@ -25,6 +25,7 @@ dict = {'ip': routerIP, 'username': user, 'password': secret, 'device_type': 'ci
 
 try:
   connection = ConnectHandler(**dict)
+  output = connection.send_command('show run')
 except (NetMikoTimeoutException):
   print("The device " + routerIP + " timed out while trying to connect.")
 except (AuthenticationException):
@@ -36,7 +37,6 @@ except (EOFError):
 except Exception as other_error:
   print(" the error " + str(other_error) + " occured while connecting to " + routerIP)
 finally:
-  output = connection.send_command('show run')
   backupFile.write(output)
   backupFile.close()
   print("The script ran successfully")
